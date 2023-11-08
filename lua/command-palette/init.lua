1a397dab454af38a8db2971c59e254dba63a2ea0
AtleSkaanes <atle.skaanes@gmail.com>
2023-11-08 18:13:48 +0100

    Removed old code

--- lua/command-palette/init.lua
+++ lua/command-palette/init.lua
@@ -2,9 +2,6 @@ CmdPalette = {}
 
 CmdPalette.has_categories = true
 
-CmdPalette.cmdNames = {}
-CmdPalette.cmdFuncs = {}
-
 -- default args
 CmdPalette.args = {
     commands = {},
@@ -18,16 +15,11 @@ function CmdPalette.setup(opts)
     if #CmdPalette.args.commands <= 1 and CmdPalette.args.commands[1].name == nil then
         CmdPalette.has_categories = false
     end
-    local i = 1
-    for _, v in pairs(CmdPalette.args.commands) do
-        CmdPalette.cmdNames[i] = v.name
-        CmdPalette.cmdFuncs[i] = v.cmd
-        i = i + 1
-    end
 end
 
 -- Function 
 function CmdPalette.show()
+    
 end
 
 -- Execute a specific command from opts
