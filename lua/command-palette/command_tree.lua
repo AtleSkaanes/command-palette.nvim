9aa4d92943499a25893a8d0a3d3d1309c61f5f0c
AtleSkaanes <atle.skaanes@gmail.com>
2023-11-10 18:43:13 +0100

    Small fixes

--- lua/command-palette/command_tree.lua
+++ lua/command-palette/command_tree.lua
@@ -4,7 +4,7 @@ local M = {}
 local convert_to_tree = function(commands)
 	local tree = {}
 	for _, v in pairs(commands) do
-		if v.category ~= nil then
+		if v.category ~= nil or v.category == "" or v.category == " " then
 			if tree[v.category] == nil then
 				tree[v.category] = {}
 			end
@@ -50,9 +50,21 @@ function M.get_commands(category)
 		t = get_commands_from_branch(tree)
 	elseif tree[category] ~= nil then
 		t = get_commands_from_branch(tree[category])
-		table.insert(t, 1, "...")
+		table.insert(t, 1, "..")
 	end
 	return t
+end
+
+---@param name string
+---@param category? string
+function M.get_cmd_by_name(name, category)
+	local commands = require("command-palette").opts.commands
+	for _, v in commands do
+		if v.name == name and v.category == category then
+			return v
+		end
+	end
+	return nil
 end
 
 return M
