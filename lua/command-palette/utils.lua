77de007b8b51902cec0f4b54df2950e68f75b631
Christoffer Hald Christensen <chrishald@outlook.dk>
2023-11-12 01:19:17 +0100

    Added a tests folder and made a overide function

--- lua/command-palette/utils.lua
+++ lua/command-palette/utils.lua
@@ -1,5 +1,20 @@
 local M = {}
 
+function M.dump(o)
+	if type(o) == "table" then
+		local s = "{ "
+		for k, v in pairs(o) do
+			if type(k) ~= "number" then
+				k = '"' .. k .. '"'
+			end
+			s = s .. "[" .. k .. "] = " .. M.dump(v) .. ","
+		end
+		return s .. "} "
+	else
+		return tostring(o)
+	end
+end
+
 ---Copies a table recursivly copied from lua docs
 ---@param orig any # table to be copied
 ---@param copies? table # dunt know
@@ -38,6 +53,35 @@ function M.split_str(input, seperator)
 		table.insert(t, str)
 	end
 	return t
+end
+
+---Overrides the orig table with the values from the override table
+---@param orig any | table # the table or value to override
+---@param override table # the table with the overide values
+---@return table # the overwritten table
+function M.deep_override(orig, override)
+	-- No table to override, just return the overide
+	if type(orig) ~= "table" then
+		return override
+	end
+
+	-- Copy the table so not to change original
+	local new = M.deepcopy(orig)
+	for key, value in pairs(override) do
+		if type(key) == "number" then
+			table.insert(new, value)
+			goto continue
+		end
+		-- If the value is a table override that as well
+		if type(value) == "table" then
+			new[key] = M.deep_override(new[key], value)
+		elseif value ~= nil then -- If the value is not a table set the value directly
+			new[key] = value
+		end
+		::continue::
+	end
+
+	return new
 end
 
 return M
